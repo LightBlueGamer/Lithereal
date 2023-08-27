5f0cca4e02f5389bffe1f2b14e673ca5590eca5b
Alexandra-Myers <bia416cat@gmail.com>
2023-08-27 13:37:25 -0400

    forgor

--- src/main/java/org/litecraft/lithereal/client/renderer/InfusedLitheriteBlockEntityModel.java
+++ src/main/java/org/litecraft/lithereal/client/renderer/InfusedLitheriteBlockEntityModel.java
@@ -12,24 +12,24 @@ import net.minecraft.resources.ResourceLocation;
 
 public class InfusedLitheriteBlockEntityModel extends Model {
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("lithereal", "infused_litherite_block"), "main");
-    private final ModelPart main;
+    private final ModelPart body;
 
     public InfusedLitheriteBlockEntityModel(ModelPart root) {
         super(RenderType::entityCutoutNoCull);
-        this.main = root.getChild("main");
+        this.body = root.getChild("body");
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        main.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
--- src/main/java/org/litecraft/lithereal/event/ClientEvents.java
+++ src/main/java/org/litecraft/lithereal/event/ClientEvents.java
@@ -7,6 +7,7 @@ import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.litecraft.lithereal.Lithereal;
+import org.litecraft.lithereal.client.renderer.InfusedLitheriteBlockEntityModel;
 import org.litecraft.lithereal.client.renderer.InfusedLitheriteBlockEntityRenderer;
 import org.litecraft.lithereal.util.KeyBinding;
 
@@ -21,6 +22,10 @@ public class ClientEvents {
 
     @Mod.EventBusSubscriber(modid = Lithereal.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void modelLayerLocationInit(EntityRenderersEvent.RegisterLayerDefinitions event) {
+            event.registerLayerDefinition(InfusedLitheriteBlockEntityModel.LAYER_LOCATION, InfusedLitheriteBlockEntityModel::createBodyLayer);
+        }
         @SubscribeEvent
         public static void onKeyRegister(RegisterKeyMappingsEvent event) {
             event.register(KeyBinding.SCORCH_KEY);
