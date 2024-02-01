917629791a47f60b0cdd46dc16d3cb0fb756701c
stunfmc <austinkemerer@gmail.com>
2024-02-01 11:09:19 -0500

    Fix forge crash

--- forge/src/main/java/org/lithereal/forge/LitherealExpectPlatformImpl.java
+++ forge/src/main/java/org/lithereal/forge/LitherealExpectPlatformImpl.java
@@ -101,5 +101,6 @@ public class LitherealExpectPlatformImpl {
     }
 
     public static BlockEntityType<LitherCollectorBlockEntity> getLitherCollectorBlockEntity() {
+        return null;
     }
 }
