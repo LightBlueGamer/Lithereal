13967e3f82f63ac4cb0d7f7a6d289c0ea4efba09
stunfmc <austinkemerer@gmail.com>
2024-02-06 17:05:21 -0500

    Add Wrench & small bug fixes

--- common/src/main/java/org/lithereal/block/entity/LitherCollectorBlockEntity.java
+++ common/src/main/java/org/lithereal/block/entity/LitherCollectorBlockEntity.java
@@ -3,30 +3,71 @@ package org.lithereal.block.entity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 import org.lithereal.LitherealExpectPlatform;
+import org.lithereal.screen.FreezingStationMenu;
 import org.lithereal.util.LitherEnergyContainer;
 
 public class LitherCollectorBlockEntity extends BlockEntity implements MenuProvider {
-    public LitherCollectorBlockEntity(BlockPos blockPos, BlockState blockState) {
-        super(LitherealExpectPlatform.getLitherCollectorBlockEntity(), blockPos, blockState);
-    }
+    protected final ContainerData data;
+    protected int progress = 0;
+    protected int maxProgress = 200;
 
-    private final LitherEnergyContainer ENERGY_CONTAINER = new LitherEnergyContainer(0, 10000, 20);
+    public LitherCollectorBlockEntity(BlockPos pos, BlockState state) {
+        super(LitherealExpectPlatform.getLitherCollectorBlockEntity(), pos, state);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch (index) {
+                    case 0 -> LitherCollectorBlockEntity.this.progress;
+                    case 1 -> LitherCollectorBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch (index) {
+                    case 0 -> LitherCollectorBlockEntity.this.progress = value;
+                    case 1 -> LitherCollectorBlockEntity.this.maxProgress = value;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        };
+    }
 
     @Override
     public Component getDisplayName() {
-        return null;
+        return Component.literal("Lither Collector");
+    }
+
+    protected void resetProgress() {
+        this.progress = 0;
+    }
+
+    protected static boolean canInsertItemIntoOutput(SimpleContainer inventory, ItemStack itemStack) {
+        return inventory.getItem(2).getItem() == itemStack.getItem() || inventory.getItem(2).isEmpty();
+    }
+
+    protected static boolean canInsertAmountIntoOutput(SimpleContainer inventory) {
+        return inventory.getItem(2).getMaxStackSize() > inventory.getItem(2).getCount();
     }
 
     @Nullable
     @Override
     public AbstractContainerMenu createMenu(int i, Inventory inventory, Player player) {
-        return null;
+        return new FreezingStationMenu(i, inventory, this, this.data);
     }
 }
