05e648d709e225868388934ec9fa512f77ddc761
stunfmc <austinkemerer@gmail.com>
2024-02-29 15:34:34 -0500

    Bug fixes & Lither Charge optimization

--- common/src/main/java/org/lithereal/entity/ThrownLitherCharge.java
+++ common/src/main/java/org/lithereal/entity/ThrownLitherCharge.java
@@ -1,26 +1,24 @@
 package org.lithereal.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.nbt.CompoundTag;
 
 public class ThrownLitherCharge extends ThrowableItemProjectile {
-
-    public ThrownLitherCharge(EntityType<? extends net.minecraft.world.entity.projectile.ThrownEnderpearl> arg, Level arg2) {
-        super(arg, arg2);
-    }
 
     public ThrownLitherCharge(Level arg, LivingEntity arg2) {
         super(EntityType.ENDER_PEARL, arg2, arg);
@@ -44,7 +42,7 @@ public class ThrownLitherCharge extends ThrowableItemProjectile {
                 }
 
                 if (blockState.getBlock() != Blocks.AIR) {
-                    if (this.getOwner() instanceof Player && !((Player)this.getOwner()).isSpectator()) {
+                    if (this.getOwner() instanceof Player && !((Player) this.getOwner()).isSpectator()) {
                         if (!(this.getOwner().getXRot() > 70 && this.getOwner().getXRot() < 110)) {
                             double fallDistance = this.getOwner().getY() - blockPos.getY();
 
@@ -56,6 +54,40 @@ public class ThrownLitherCharge extends ThrowableItemProjectile {
                         this.getOwner().teleportTo(blockPos.getX(), blockPos.getY(), blockPos.getZ());
                     }
                 }
+            } else if (hitResult.getType() == HitResult.Type.ENTITY) {
+                EntityHitResult entityHitResult = (EntityHitResult) hitResult;
+                if (entityHitResult.getEntity() instanceof Player) {
+                    Player playerHit = (Player) entityHitResult.getEntity();
+                    if (this.getOwner() instanceof Player && playerHit == this.getOwner() && !playerHit.isSpectator()) {
+                        return;
+                    }
+                }
+
+                LivingEntity target = (LivingEntity) entityHitResult.getEntity();
+
+                float damageAmount = 2.0f;
+                target.hurt(this.damageSources().thrown(this, this.getOwner()), damageAmount);
+
+                if (!this.getCommandSenderWorld().isClientSide) {
+                    if (this.getOwner() != null && this.getOwner() instanceof LivingEntity && this.getOwner() != target) {
+                        this.discard();
+                    }
+                    double explosionPower = 1.0;
+                    this.getCommandSenderWorld().explode(null, target.getX(), target.getY(), target.getZ(), (float) explosionPower, Level.ExplosionInteraction.NONE);
+
+                    double xDiff = target.getX() - this.getX();
+                    double zDiff = target.getZ() - this.getZ();
+                    double distance = Math.sqrt(xDiff * xDiff + zDiff * zDiff);
+
+                    double knockbackStrength = 1.0;
+                    if (distance > 0) {
+                        double normalizedX = xDiff / distance;
+                        double normalizedZ = zDiff / distance;
+                        target.push(normalizedX * knockbackStrength, 0.0, normalizedZ * knockbackStrength);
+
+                        target.setDeltaMovement(target.getDeltaMovement().x, 0.5, target.getDeltaMovement().z);
+                    }
+                }
             }
             this.discard();
         }
@@ -63,12 +95,25 @@ public class ThrownLitherCharge extends ThrowableItemProjectile {
 
     protected void onHitEntity(EntityHitResult arg) {
         super.onHitEntity(arg);
-        LivingEntity target = (LivingEntity)arg.getEntity();
+        LivingEntity target = (LivingEntity) arg.getEntity();
+
+        if (this.getOwner() instanceof Player && target == this.getOwner()) {
+            return;
+        }
 
         float damageAmount = 2.0f;
         target.hurt(this.damageSources().thrown(this, this.getOwner()), damageAmount);
 
         if (!this.getCommandSenderWorld().isClientSide) {
+            if (target instanceof Player && !target.getCommandSenderWorld().isClientSide) {
+                Player playerTarget = (Player) target;
+                ItemStack activeItemStack = playerTarget.getUseItem();
+                if (!activeItemStack.isEmpty() && activeItemStack.getItem() instanceof ShieldItem) {
+                    playerTarget.getCooldowns().addCooldown(activeItemStack.getItem(), 100); // 100 ticks = 5 seconds
+                    return;
+                }
+            }
+
             if (this.getOwner() != null && this.getOwner() instanceof LivingEntity && this.getOwner() != target) {
                 this.discard();
             }
