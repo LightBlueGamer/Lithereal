1090a515a3567c9760ce2c8d097618746a295cde
stunfmc <austinkemerer@gmail.com>
2024-03-22 15:00:58 -0400

    War Hammer fixes

--- common/src/main/java/org/lithereal/item/custom/WarHammer.java
+++ common/src/main/java/org/lithereal/item/custom/WarHammer.java
@@ -27,6 +27,10 @@ public class WarHammer extends SwordItem {
 
                 float knockbackStrength = 1.0F;
 
+                stack.hurtAndBreak(1, player, (entity) -> {
+                    entity.broadcastBreakEvent(player.getUsedItemHand());
+                });
+
                 if (player.isUsingItem()) {
                     handleSweepAttack(player, target, knockbackStrength);
                 } else {
@@ -60,13 +64,20 @@ public class WarHammer extends SwordItem {
     }
 
     private void applyKnockbackToNearbyEntities(Player player, LivingEntity target, float knockbackStrength) {
-        double radius = 5.0;
+        double radius = 3.0;
         List<LivingEntity> nearbyEntities = player.getCommandSenderWorld().getEntitiesOfClass(LivingEntity.class, target.getBoundingBox().inflate(radius));
         nearbyEntities.remove(target);
+
+        int affectedEntities = 0;
 
         for (LivingEntity nearbyEntity : nearbyEntities) {
             if (!nearbyEntity.isCrouching() && !nearbyEntity.isFallFlying() && Math.abs(nearbyEntity.getY() - target.getY()) < 0.1) {
                 nearbyEntity.knockback(knockbackStrength, Mth.sin(player.getYRot() * ((float) Math.PI / 180F)), -Mth.cos(player.getYRot() * ((float) Math.PI / 180F)));
+                affectedEntities++;
+
+                if (affectedEntities >= 4) {
+                    break;
+                }
             }
         }
     }
