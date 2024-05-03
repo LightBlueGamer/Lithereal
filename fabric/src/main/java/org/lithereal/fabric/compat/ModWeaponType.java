d425fa097463e040f0f93858c9c8ea18c603a14a
Alexandra-Myers <bia416cat@gmail.com>
2024-05-02 22:57:43 -0400

    combatify update

--- fabric/src/main/java/org/lithereal/fabric/compat/ModWeaponType.java
+++ fabric/src/main/java/org/lithereal/fabric/compat/ModWeaponType.java
@@ -1,5 +1,6 @@
 package org.lithereal.fabric.compat;
 
+import net.atlas.combatify.Combatify;
 import net.atlas.combatify.item.WeaponType;
 
 import static net.atlas.combatify.item.WeaponType.*;
@@ -8,6 +9,7 @@ public class ModWeaponType {
     public static final WeaponType HAMMER = createBasic("hammer", 3.0, -0.75, 0.5);
     public static final WeaponType WAR_HAMMER = createBasic("war_hammer", 3.0, -0.5, 0);
     public static void init() {
-
+        Combatify.defineDefaultWeaponType(HAMMER);
+        Combatify.defineDefaultWeaponType(WAR_HAMMER);
     }
 }
