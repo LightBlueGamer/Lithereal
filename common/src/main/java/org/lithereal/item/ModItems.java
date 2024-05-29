47dcf4d9d9246e7b4b145c62b3c3e16e67acb5d8
stunfmc <austinkemerer@gmail.com>
2024-05-29 16:46:51 -0400

    Update War Hammer

--- common/src/main/java/org/lithereal/item/ModItems.java
+++ common/src/main/java/org/lithereal/item/ModItems.java
@@ -157,7 +157,7 @@ public class ModItems {
             new MysteriousItem(new Item.Properties().fireResistant()));
 
     public static final RegistrySupplier<Item> WAR_HAMMER = ITEMS.register("war_hammer", () ->
-            LitherealExpectPlatform.createWarHammer(ODYSIUM, 1, -3.5f, new Item.Properties().durability(512).fireResistant()));
+            LitherealExpectPlatform.createWarHammer(ODYSIUM, 1, -3.4f, new Item.Properties().durability(512).fireResistant()));
 
     public static final RegistrySupplier<Item> LITHERITE_BRUSH = ITEMS.register("litherite_brush", () ->
             new LitheriteBrushItem(new Item.Properties().durability(256)));
