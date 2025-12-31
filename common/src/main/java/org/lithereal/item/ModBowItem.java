ce20227a0a2efe84b4dca378082a297d270600fb
Alexandra-Myers <bia416cat@gmail.com>
2025-12-30 19:34:20 -0500

    bow

--- common/src/main/java/org/lithereal/item/ModBowItem.java
+++ common/src/main/java/org/lithereal/item/ModBowItem.java
@@ -4,15 +4,19 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Tier;
 import org.jetbrains.annotations.Nullable;
 
 public class ModBowItem extends BowItem {
+    private final Tier tier;
     private final float speedFactor;
     private final float fatigueFactor;
     private final float damageFactor;
 
-    public ModBowItem(Properties properties, float speedFactor, float fatigueFactor, float damageFactor) {
-		super(properties);
+    public ModBowItem(Tier tier, Properties properties, float speedFactor, float fatigueFactor, float damageFactor) {
+		super(properties.durability(tier.getUses()));
+        this.tier = tier;
         this.speedFactor = speedFactor;
         this.fatigueFactor = fatigueFactor;
         this.damageFactor = damageFactor;
@@ -27,5 +31,15 @@ public class ModBowItem extends BowItem {
     @Override
     public int getDefaultProjectileRange() {
         return (int) (super.getDefaultProjectileRange() * speedFactor);
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.tier.getEnchantmentValue();
+    }
+
+    @Override
+    public boolean isValidRepairItem(ItemStack itemStack, ItemStack itemStack2) {
+        return this.tier.getRepairIngredient().test(itemStack2) || super.isValidRepairItem(itemStack, itemStack2);
     }
 }
