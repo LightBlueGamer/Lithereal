2b29d406de3befec227f91de10d20fac706199ca
Alexandra-Myers <bia416cat@gmail.com>
2026-06-14 17:03:12 -0400

    add item models for previously missing

--- src/main/java/org/lithereal/neoforge/datagen/ModModelProvider.java
+++ src/main/java/org/lithereal/neoforge/datagen/ModModelProvider.java
@@ -52,27 +52,37 @@ public class ModModelProvider extends ModelProvider {
         blockWithItem(ModBlocks.PURE_ETHER_SOURCE, blockModels);
         blockWithItem(ModBlocks.ETHEREAL_CRYSTAL_BLOCK, blockModels);
 
+        itemForBlockModel(ModTreeBlocks.PHANTOM_OAK_PLANKS.get(), blockModels);
         blockModels.createPlantWithDefaultItem(ModTreeBlocks.PHANTOM_OAK_SAPLING.get(), ModTreeBlocks.POTTED_PHANTOM_OAK_SAPLING.get(), BlockModelGenerators.PlantType.NOT_TINTED);
         logBlock(ModTreeBlocks.PHANTOM_OAK_LOG.get(),  ModTreeBlocks.PHANTOM_OAK_WOOD.get(), blockModels);
         logBlock(ModTreeBlocks.STRIPPED_PHANTOM_OAK_LOG.get(), ModTreeBlocks.STRIPPED_PHANTOM_OAK_WOOD.get(), blockModels);
         blockModels.createHangingSign(ModTreeBlocks.STRIPPED_PHANTOM_OAK_LOG.get(), ModTreeBlocks.PHANTOM_OAK_HANGING_SIGN.get(), ModTreeBlocks.PHANTOM_OAK_WALL_HANGING_SIGN.get());
         blockModels.createTrivialBlock(ModTreeBlocks.PHANTOM_OAK_LEAVES.get(), TexturedModel.LEAVES);
         itemForBlockModel(ModTreeBlocks.PHANTOM_OAK_LEAVES.get(), blockModels);
+        itemForBlockModel(ModTreeBlocks.PHANTOM_OAK_FENCE_GATE.get(), blockModels);
+        itemForBlockModel(ModTreeBlocks.PHANTOM_OAK_PRESSURE_PLATE.get(), blockModels);
 
         blockWithItem(ModTreeBlocks.MALISHROOM_BLOCK, blockModels);
         blockWithItem(ModTreeBlocks.RED_MALISHROOM_BLOCK, blockModels);
+        itemForBlockModel(ModTreeBlocks.MALISHROOM_PLANKS.get(), blockModels);
         logBlock(ModTreeBlocks.MALISHROOM_STEM.get(), ModTreeBlocks.MALISHROOM_HYPHAE.get(), blockModels);
         logBlock(ModTreeBlocks.STRIPPED_MALISHROOM_STEM.get(), ModTreeBlocks.STRIPPED_MALISHROOM_HYPHAE.get(), blockModels);
         blockModels.createHangingSign(ModTreeBlocks.STRIPPED_MALISHROOM_STEM.get(), ModTreeBlocks.MALISHROOM_HANGING_SIGN.get(), ModTreeBlocks.MALISHROOM_WALL_HANGING_SIGN.get());
+        itemForBlockModel(ModTreeBlocks.MALISHROOM_FENCE_GATE.get(), blockModels);
+        itemForBlockModel(ModTreeBlocks.MALISHROOM_PRESSURE_PLATE.get(), blockModels);
 
         blockWithItem(ModTreeBlocks.FORTSHROOM_BLOCK, blockModels);
+        itemForBlockModel(ModTreeBlocks.FORTSHROOM_PLANKS.get(), blockModels);
         logBlock(ModTreeBlocks.FORTSHROOM_STEM.get(), ModTreeBlocks.FORTSHROOM_HYPHAE.get(), blockModels);
         logBlock(ModTreeBlocks.STRIPPED_FORTSHROOM_STEM.get(), ModTreeBlocks.STRIPPED_FORTSHROOM_HYPHAE.get(), blockModels);
         blockModels.createHangingSign(ModTreeBlocks.STRIPPED_FORTSHROOM_STEM.get(), ModTreeBlocks.FORTSHROOM_HANGING_SIGN.get(), ModTreeBlocks.FORTSHROOM_WALL_HANGING_SIGN.get());
+        itemForBlockModel(ModTreeBlocks.FORTSHROOM_FENCE_GATE.get(), blockModels);
+        itemForBlockModel(ModTreeBlocks.FORTSHROOM_PRESSURE_PLATE.get(), blockModels);
 
         blockModels.createPlantWithDefaultItem(ModVegetationBlocks.MALISHROOM.get(), ModVegetationBlocks.POTTED_MALISHROOM.get(), BlockModelGenerators.PlantType.NOT_TINTED);
         blockModels.createPlantWithDefaultItem(ModVegetationBlocks.FORTSHROOM.get(), ModVegetationBlocks.POTTED_FORTSHROOM.get(), BlockModelGenerators.PlantType.NOT_TINTED);
 
+        itemForBlockModel(ModStoneBlocks.ETHERSTONE.get(), blockModels);
         blockModels.family(ModStoneBlocks.ETHERSTONE.get()).generateFor(new BlockFamily.Builder(ModStoneBlocks.ETHERSTONE.get())
                 .wall(ModStoneBlocks.ETHERSTONE_WALL.get())
                 .stairs(ModStoneBlocks.ETHERSTONE_STAIRS.get())
@@ -81,7 +91,15 @@ public class ModModelProvider extends ModelProvider {
                 .getFamily());
         blockModels.createAxisAlignedPillarBlock(ModStoneBlocks.CHISELED_ETHERSTONE.get(), TexturedModel.COLUMN);
         itemForBlockModel(ModStoneBlocks.CHISELED_ETHERSTONE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_ETHERSTONE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.ETHERSTONE_BRICKS.get(), blockModels);
 
+        itemForBlockModel(ModStoneBlocks.PAILITE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.PAILITE_PRESSURE_PLATE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_PAILITE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_PAILITE_PRESSURE_PLATE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_PAILITE_BRICKS.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_PAILITE_BRICK_PRESSURE_PLATE.get(), blockModels);
         blockModels.family(ModStoneBlocks.POLISHED_PAILITE_BRICKS.get()).generateFor(new BlockFamily.Builder(ModStoneBlocks.POLISHED_PAILITE_BRICKS.get())
                 .wall(ModStoneBlocks.POLISHED_PAILITE_BRICK_WALL.get())
                 .stairs(ModStoneBlocks.POLISHED_PAILITE_BRICK_STAIRS.get())
@@ -91,6 +109,12 @@ public class ModModelProvider extends ModelProvider {
                 .getFamily());
         blockModels.createAxisAlignedPillarBlock(ModStoneBlocks.CHISELED_POLISHED_PAILITE_BRICKS.get(), TexturedModel.COLUMN);
         itemForBlockModel(ModStoneBlocks.CHISELED_POLISHED_PAILITE_BRICKS.get(), blockModels);
+
+        itemForBlockModel(ModStoneBlocks.VERDONE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_VERDONE.get(), blockModels);
+
+        itemForBlockModel(ModStoneBlocks.LUMINITE.get(), blockModels);
+        itemForBlockModel(ModStoneBlocks.POLISHED_LUMINITE.get(), blockModels);
 
         blockWithItem(ModOreBlocks.LITHERITE_ORE, blockModels);
         blockWithItem(ModOreBlocks.DEEPSLATE_LITHERITE_ORE, blockModels);
