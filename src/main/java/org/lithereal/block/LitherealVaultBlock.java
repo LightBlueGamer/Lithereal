a8d4de45fb5f38af5a0e8ab82a60bed09bb78e2b
Alexandra-Myers <bia416cat@gmail.com>
2026-06-15 19:53:30 -0400

    fix vaults

--- src/main/java/org/lithereal/block/LitherealVaultBlock.java
+++ src/main/java/org/lithereal/block/LitherealVaultBlock.java
@@ -3,24 +3,17 @@ package org.lithereal.block;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.VaultBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.trialspawner.PlayerDetector;
 import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
-import net.minecraft.world.level.block.entity.vault.VaultConfig;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.lithereal.Lithereal;
 import org.lithereal.block.entity.LitherealVaultBlockEntity;
 import org.lithereal.block.entity.ModBlockEntities;
-import org.lithereal.item.ModItems;
-
-import java.util.Optional;
 
 public class LitherealVaultBlock extends VaultBlock {
     public static final MapCodec<VaultBlock> CODEC = simpleCodec(LitherealVaultBlock::new);
@@ -35,9 +28,7 @@ public class LitherealVaultBlock extends VaultBlock {
 
     @Nullable
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
-        LitherealVaultBlockEntity ret = new LitherealVaultBlockEntity(blockPos, blockState);
-        ret.setConfig(new VaultConfig(Lithereal.LITHEREAL_CHAMBERS_REWARD, 4.0, 4.5, new ItemStack(ModItems.LITHEREAL_KEY), Optional.empty(), PlayerDetector.INCLUDING_CREATIVE_PLAYERS, PlayerDetector.EntitySelector.SELECT_FROM_LEVEL));
-        return ret;
+        return new LitherealVaultBlockEntity(blockPos, blockState);
     }
 
     @Nullable
--- src/main/java/org/lithereal/block/entity/LitherealVaultBlockEntity.java
+++ src/main/java/org/lithereal/block/entity/LitherealVaultBlockEntity.java
@@ -1,14 +1,27 @@
 package org.lithereal.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
+import net.minecraft.world.level.block.entity.vault.VaultConfig;
 import net.minecraft.world.level.block.state.BlockState;
+import org.lithereal.Lithereal;
+import org.lithereal.block.LitherealVaultBlock;
+import org.lithereal.item.ModItems;
+
+import java.util.Optional;
 
 public class LitherealVaultBlockEntity extends VaultBlockEntity {
-
+    public static final VaultConfig DEFAULT =
+            new VaultConfig(Lithereal.LITHEREAL_CHAMBERS_REWARD,
+                    4.0,
+                    4.5,
+                    new ItemStack(ModItems.LITHEREAL_KEY.get()),
+                    Optional.empty());
     public LitherealVaultBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(blockPos, blockState);
         ((IBlockEntity) this).setType(ModBlockEntities.LITHEREAL_VAULT.get());
+        setConfig(DEFAULT);
     }
 
     @Override
